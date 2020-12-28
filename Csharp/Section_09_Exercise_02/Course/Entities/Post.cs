3d6d0ac6e68d42b3a5bac38f081d3bd5ee59a7db
William Rezende <williamrezendee@gmail.com>
2020-12-27 23:02:05 -0300

    Add a new exercise in Csharp folder

---
+++ Csharp/Section_09_Exercise_02/Course/Entities/Post.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Course.Entities
+{
+    class Post
+    {
+        public DateTime Moment { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public int Likes { get; set; }
+        public List<Comment> Comments { get; set; } = new List<Comment>();
+
+        public Post()
+        {
+        }
+
+        public Post(DateTime moment, string title, string content, int likes)
+        {
+            Moment = moment;
+            Title = title;
+            Content = content;
+            Likes = likes;
+        }
+
+        public void AddComment(Comment comnt)
+        {
+            Comments.Add(comnt);
+        }
+
+        public void RemoveComment(Comment comnt)
+        {
+            Comments.Remove(comnt);
+        }
+
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.AppendLine(Title);
+            stringBuilder.Append(Likes);
+            stringBuilder.Append(" Likes - ");
+            stringBuilder.AppendLine(Moment.ToString("dd/MM/yyyy HH:mm:ss"));
+            stringBuilder.AppendLine(Content);
+            stringBuilder.AppendLine("Comments:");
+            foreach (Comment cmt in Comments)
+            {
+                stringBuilder.AppendLine(cmt.Text);
+            }
+
+            return stringBuilder.ToString();
+        }
+    }
+}
