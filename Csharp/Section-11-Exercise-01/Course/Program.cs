f7c5deb71d45f90363c9945c3171b8ca5507b174
William Rezende <williamrezendee@gmail.com>
2021-01-28 19:53:37 -0300

    Atualizando Exercício 11

---
+++ Csharp/Section-11-Exercise-01/Course/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Course.Entities;
+using Course.Entities.Exceptions;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Room number: ");
+                int room = int.Parse(Console.ReadLine());
+                Console.Write("Check-in date (dd/MM/yyyy): ");
+                DateTime checkIn = DateTime.Parse(Console.ReadLine());
+                Console.Write("Check-out date (dd/MM/yyyy): ");
+                DateTime checkOut = DateTime.Parse(Console.ReadLine());
+
+                Reservation reservation = new Reservation(room, checkIn, checkOut);
+                Console.WriteLine("Reservation : " + reservation);
+
+                Console.WriteLine();
+                Console.WriteLine("Enter data to update the reservation:");
+                Console.Write("Check-in date (dd/MM/yyyy): ");
+                checkIn = DateTime.Parse(Console.ReadLine());
+                Console.Write("Check-out date (dd/MM/yyyy): ");
+                checkOut = DateTime.Parse(Console.ReadLine());
+
+                reservation.UpdateDates(checkIn, checkOut);
+                Console.WriteLine("Reservation: " + reservation);
+            }
+            catch (DomainException e)
+            {
+                Console.WriteLine("Error in reservation: " + e.Message);
+            }
+        }
+    }
+}
