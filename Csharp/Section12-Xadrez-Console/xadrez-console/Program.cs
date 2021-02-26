83e776d894793abc5464219da141b25501b16614
William Rezende <williamrezendee@gmail.com>
2021-02-25 21:57:02 -0300

    Criando projeto xadrez-console

---
+++ Csharp/Section12-Xadrez-Console/xadrez-console/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using tabuleiro;
+
+namespace xadrez_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tabuleiro tab = new Tabuleiro(8, 8);
+        }
+    }
+}
---
+++ Csharp/Section12-Xadrez-Console/xadrez-console/tabuleiro/Peca.cs
@@ -0,0 +1,18 @@
+﻿namespace tabuleiro
+{
+    class Peca
+    {
+        public Posicao posicao { get; set; }
+        public Cor  cor { get; protected set; }
+        public int quantidadeMovimentos { get; protected set; }
+        public Tabuleiro tabuleiro { get; protected set; }
+
+        public Peca(Posicao pos, Tabuleiro tab, Cor cor)
+        {
+            this.posicao = pos;
+            this.tabuleiro = tab;
+            this.cor = cor;
+            this.quantidadeMovimentos = 0;
+        }
+    }
+}
