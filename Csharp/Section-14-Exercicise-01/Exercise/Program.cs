700c77b2f83827223684d954ec6cdb2894c8228b
William Rezende <williamrezendee@gmail.com>
2021-04-19 21:30:34 -0300

    Iniciando codigo do exercicio 1 secao 14

---
+++ Csharp/Section-14-Exercicise-01/Exercise/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter contract data");
+            Console.Write("Number: ");
+            int contractNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine("Date (dd/MM/yyyy): ");
+            DateTime contractDate = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy", CultureInfo.InvariantCulture);
+            Console.WriteLine("Contract value: ");
+            double contractValue = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Enter numer of installments: ");
+            int installments = int.Parse(Console.ReadLine());
+        }
+    }
+}
